70b60b12af81e932adda81cddc83667c8e95cfad
Franziska Kroeger <kroeger.franziska97@gmail.com>
2022-11-15 20:11:45 +0100

    limpiezaDatos-15/11

--- LimpiezaDeDatos/influencers.py
+++ LimpiezaDeDatos/influencers.py
@@ -2,19 +2,22 @@
 import pandas as pd
 import os  
 
+
+
+
 # Leer los datos
-df = pd.read_csv('../Datos/influencers.csv')
+df = pd.read_csv('../Datos/instaInfluencers.csv')
 #print(df)
 
+
+## ESTO ESTA MAL; HAY QUE ANIADIR EL ID 
 # Extraer columnas
-extractCol = df[["Rank", "Account", "Followers", "Authentic engagement"]]
+extractCol = df[["username", "Followers", "Likes Avg."]]
+extractCol['influencer_id'] = id(df)
 #print(extractCol)
 
 
-# Renombrar el rank al influencer_id
-influencer = extractCol.rename(columns={'Rank': 'influencer_id'}) 
-#print(influencer)
 
 # Crear csv's en un nuevo directorio
 os.makedirs('../DatosLimpios', exist_ok=True)  
-influencer.to_csv('../DatosLimpios/influencer.csv')  
+extractCol.to_csv('../DatosLimpios/influencer.csv')  
