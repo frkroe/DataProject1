53075ba8b8bebe96b020ded51c873016412cddfe
Franziska Kroeger <kroeger.franziska97@gmail.com>
2022-11-23 19:04:09 +0100

    update-23.11
    finalisation of .py files

---
+++ main.py
@@ -0,0 +1,13 @@
+# no funciona!!!!
+
+# Importaciones
+from LimpiezaDeDatos.rawdata.muebles import muebles
+from LimpiezaDeDatos.rawdata.influencers import influencer
+from LimpiezaDeDatos.rawdata.clientes import clientes
+from LimpiezaDeDatos.composicion import composicion
+
+# Dataframes
+df_muebles = muebles()
+df_influencer = influencer()
+df_clientes = clientes()
+df_composicion = composicion()
