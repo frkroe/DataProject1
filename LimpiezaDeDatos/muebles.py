73b3382675cf05f33f18d22df4d6c75427dc4156
Alejandro <alejandromartilledo1995@gmail.com>
2022-11-24 18:05:23 +0100

    Update muebles.py

--- LimpiezaDeDatos/muebles.py
+++ LimpiezaDeDatos/muebles.py
@@ -19,9 +19,9 @@ def muebles():
 
     # Crear csv's en un nuevo directorio
     os.makedirs('../DatosLimpios', exist_ok=True)  
-    muebles.to_csv('../DatosLimpios/muebles.csv')  
+    muebles.to_csv('../DatosLimpios/muebles/muebles.csv')  
 
-    items = pd.read_csv('../DatosLimpios/muebles.csv', index_col=0).to_dict()
+    items = pd.read_csv('../DatosLimpios/muebles/muebles.csv', index_col=0).to_dict()
     key, value = "cat_id", []
     items[key] = value
 
@@ -31,10 +31,10 @@ def muebles():
         items['cat_id'].append(cat)
 
     categ_id = pd.DataFrame(items['cat_id'], columns = ["cat_id"])
-    categ_id.to_csv('../DatosLimpios/muebles2.csv')
+    categ_id.to_csv('../DatosLimpios/muebles/muebles2.csv')
 
-    a = pd.read_csv("../DatosLimpios/muebles.csv")
-    b = pd.read_csv("../DatosLimpios/muebles2.csv")
+    a = pd.read_csv("../DatosLimpios/muebles/muebles.csv")
+    b = pd.read_csv("../DatosLimpios/muebles/muebles2.csv")
     b = b.dropna(axis=1)
     merged = a.merge(b)
     merged.to_csv("../DatosLimpios/cat_muebles.csv",index=False)
