66ed0d5de7c86e25e9749d78a2d3347fea6adf36
Alejandro <alejandromartilledo1995@gmail.com>
2022-11-24 21:00:15 +0100

    rev

---
+++ LimpiezaDeDatos/cat_id.py
@@ -0,0 +1,24 @@
+def cat_id():
+    import pandas as pd
+    import uuid
+
+    # Crea un csv(cat_muebles.csv) que combina los datos originales de los muebles(muebles.csv) con un cat_id(muebles.csv)
+    items = pd.read_csv('../DatosLimpios/muebles/muebles.csv', index_col=0).to_dict()
+    key, value = "cat_id", []
+    items[key] = value
+
+    id = str(uuid.uuid1())
+    for i in items['category']:
+        cat = items['category'][i][:6]+id
+        items['cat_id'].append(cat)
+
+    categ_id = pd.DataFrame(items['cat_id'], columns = ["cat_id"])
+    categ_id.to_csv('../DatosLimpios/muebles/muebles2.csv')
+
+    a = pd.read_csv("../DatosLimpios/muebles/muebles.csv")
+    b = pd.read_csv("../DatosLimpios/muebles/muebles2.csv")
+    b = b.dropna(axis=1)
+    merged = a.merge(b)
+    merged.to_csv("../DatosLimpios/cat_cmuebles.csv",index=False)
+
+cat_id()
