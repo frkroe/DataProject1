77f2f46145a0efcc34a0ea69d158c097b18dc6d4
Alejandro <alejandromartilledo1995@gmail.com>
2022-11-26 00:19:14 +0100

    Update categorias.py

--- LimpiezaDeDatos/categorias.py
+++ LimpiezaDeDatos/categorias.py
@@ -3,7 +3,8 @@ def categorias():
     import uuid
     import csv
 
-    # Crea un csv(cat_muebles.csv) que combina los datos originales de los muebles(muebles2.csv) con un cat_id(muebles.csv)
+    # Crea un csv(cat_id.csv) a partir del csv muebles
+    
     items = pd.read_csv('../DatosLimpios/muebles/muebles.csv', index_col=0).to_dict()
     key, value = "cat_id", []
     items[key] = value
